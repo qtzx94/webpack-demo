93200a4b52820bc53d8c5f422c2dfa09c46d2368
qtzx <qtzx94@gmail.com>
2019-11-05 15:21:45 +0800

    异步组件代码分割

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-import _ from 'lodash';
+function getComponent() {
+  return import('lodash').then(({ default: _ }) => {
+    var element = document.createElement('div');
+    element.innerHTML = _.join(['qtzx', '94'], '--');
+    return element;
+  })
+}
 
-console.log(_.join(['a', 'b', 'c'], '***'));
+getComponent().then(element => {
+  document.body.appendChild(element);
+});
